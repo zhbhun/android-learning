628e7ee6a90104058ef3960c2fe19dea215af02d
zhbhun <zhbhun@gmail.com>
2024-08-14 16:41:05 +0800

    docs: kotlin android 首页示例列表

---
+++ examples/kotlin/app/src/main/java/com/zhbhun/tester/kotlin/ListItemAdapter.kt
@@ -0,0 +1,32 @@
+package com.zhbhun.tester.kotlin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.zhbhun.tester.kotlin.databinding.ListItemBinding
+
+data class ListItem(val title: String, val description: String, val onClick: () -> Unit)
+
+class ListItemAdapter(private val itemList: List<ListItem>) :
+  RecyclerView.Adapter<ListItemAdapter.ListItemViewHolder>() {
+  class ListItemViewHolder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    val titleView: TextView = binding.title
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+    val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    return ListItemViewHolder(binding)
+  }
+
+  override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+    val item = itemList[position]
+    holder.titleView.text = item.title
+
+    holder.itemView.setOnClickListener {
+      item.onClick()
+    }
+  }
+
+  override fun getItemCount() = itemList.size
+}
--- examples/kotlin/app/src/main/java/com/zhbhun/tester/kotlin/MainActivity.kt
+++ examples/kotlin/app/src/main/java/com/zhbhun/tester/kotlin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.zhbhun.tester.kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zhbhun.tester.kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val recyclerView = binding.recyclerView
+    val itemList = listOf(
+      ListItem("Item 1", "Description 1") {},
+      ListItem("Item 2", "Description 2") {},
+      ListItem("Item 3", "Description 3") {},
+    )
+    val adapter = ListItemAdapter(itemList)
+    recyclerView.adapter = adapter
+    recyclerView.layoutManager = LinearLayoutManager(this)
+  }
 }
