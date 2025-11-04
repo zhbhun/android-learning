e4a04a5eee20d9870d9f1513caf66cdc0c79b895
zhbhun <zhbhun@gmail.com>
2025-11-04 20:47:17 +0800

    docs: kotlin tester

--- examples/kotlin/src/main/kotlin/Main.kt
+++ examples/kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 package com.zhbhun.kotlin
 
+import kotlinx.coroutines.*
+
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 fun main() {
   val name = "Kotlin"
+
+  runBlocking {
+    launch {
+      delay(1000L) // 模拟耗时操作
+      println("World!")
+    }
+    println("Hello,")
+  }
   //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
   // 查看 IntelliJ IDEA 建议如何修正。
   println("Hello, " + name + "!")
